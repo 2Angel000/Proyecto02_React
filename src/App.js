e0cb97038926675d8a60e5773671fc7fb7967030
2Angel000 <mscmorfy_arcangel13@hotmail.com>
2023-05-02 01:19:27 -0600

    Parte 2/13

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
+import rey1 from './imagenes/rey_atanagildo.png';
+import rey2 from './imagenes/rey_sisebuto.png';
+import {useRef} from 'react';
 import './App.css';
 
 function App() {
+  const cambio=20.15;
+  const refCaja = useRef();
+
+  //function incrementar(e){
+  const incrementar=(e)=>{
+    e.target.innerHTML = Number(e.target.innerHTML)+(1);
+
+    if(Number(e.target.innerHTML) >= 8){
+      e.target.style.backgroundColor="#FE0B0B";
+    }
+    
+    if(Number(e.target.innerHTML) == 10){
+      e.target.innerHTML = 0;
+      e.target.style.backgroundColor="#282c34";
+    }
+  }
+
+  //function convertir(){
+  const convertir=()=>{
+    //utilizando referencias: useRef [hook] => nombreRef.current
+    refCaja.current.innerHTML=Number(refCaja.current.innerHTML)*cambio;
+  }
+
+  //function cambioImagen
+  const cambioImagen=(img)=>{
+    if(img.target.src.includes("atanagildo")){
+      img.target.src=rey2;
+    }else{
+      img.target.src=rey1;
+    }
+  }
+
+  //function lectura(e){
+    const lectura=(e)=>{
+    refCaja.current.innerHTML = //para hacer el cambio en la referencia
+    e.target.//es el elemento en cuestion [input]
+    value;
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div ref={refCaja} className="caja" onClick={incrementar}>1</div>
+      <button onClick={convertir}>Convertir a MXN</button>
+      <div><img onClick={cambioImagen} src={rey1}/></div>
+      <input onChange={lectura} className="campo"/>
+    </>
   );
 }
 
